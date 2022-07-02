12dee9fa0531764b924aaa9891084c764a784f50
Liam Darkmoon <liamdarkmoon@gmail.com>
2022-07-02 15:28:27 -0300

    v2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
-const cartButtons = document.querySelectorAll('.cart-button');
+let loader = document.getElementById('loader');
 
-cartButtons.forEach(button => {
-    button.addEventListener('click', cartClick);
-});
-
-function cartClick () {
-    let button = this;
-    button.classList.add('clicked');
-}
-
-cartButtons.forEach(button => {
-    button.addEventListener('animationend', removeClick);
-});
-
-function removeClick () {
-    let button = this;
-    button.classList.remove('clicked');
-}
+window.addEventListener('load', function(){
+    loader.style.display = 'none';
+})
